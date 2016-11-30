83f2bdcb45827d48288d9d0b60f71ca179f49436
Alexander Barth <barth.alexander@gmail.com>
2016-11-30 14:12:59 +0100

    divand_operators

--- divand.jl
+++ divand.jl
@@ -2,6 +2,57 @@ module divand
 using Interpolations
 using Base.Test
 
+type divand_struct
+    n
+    sv
+    D
+    mask
+    WE
+    isinterior
+    isinterior_stag
+    isinterior_unpacked
+    mapindex_packed
+    mask_stag
+    WEs
+    Dx
+    applybc
+
+    function divand_struct(mask)
+        n = ndims(mask)
+        sv = statevector_init((mask,))
+        sz = size(mask)
+        sempty = sparse(Array{Int64}([]),Array{Int64}([]),Array{Float64}([]),prod(sz),prod(sz))
+
+        D = copy(sempty)
+        WE = copy(sempty)
+        WE = copy(sempty)
+
+        isinterior = []
+        isinterior_stag = [[] for i in 1:n]
+        isinterior_unpacked = []
+        mapindex_packed = []
+        mask_stag = [Array{Bool,1}() for i in 1:n]
+        WEs = [copy(sempty) for i in 1:n]
+        Dx = [copy(sempty) for i in 1:n]
+        applybc = copy(sempty)
+
+        new(n,
+            D,
+            sv,
+            mask,
+            WE,
+            isinterior,
+            isinterior_stag,
+            isinterior_unpacked,
+            mapindex_packed,
+            mask_stag,
+            WEs,
+            Dx,
+            applybc
+            )
+    end
+end
+
 function ndgrid_fill(a, v, s, snext)
     for j = 1:length(a)
         a[j] = v[div(rem(j-1, snext), s)+1]
@@ -53,6 +104,8 @@ include("statevector_init.jl");
 include("statevector_pack.jl");
 include("statevector_unpack.jl");
 
+include("divand_operators.jl");
+
 
 
 function test()
@@ -60,7 +113,15 @@ function test()
     include("test_localize_separable_grid.jl");
     include("test_statevector.jl");
 
+    x1,x2 = ndgrid(2*collect(1:4),3*collect(1:3))
+    mask = trues(size(x1))
+    pm = ones(size(x1))/2
+    pn = ones(size(x1))/3
+    nu = ones(size(mask))
+    iscyclic = [false,false]
+    mapindex = []
 
+    s = divand_operators(mask,(pm,pn),nu,iscyclic,mapindex)
 end
 
 export test, sparse_stagger, sparse_diff, localize_separable_grid, ndgrid, sparse_pack, sparse_interp, sparse_trim, sparse_shift, sparse_gradient, divand_laplacian,
