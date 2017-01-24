156b172e265d658a137508cd8295b998662bde75
Alexander Barth <barth.alexander@gmail.com>
2017-01-24 16:52:53 +0100

    advection contraint

---
+++ test/test_2dvar_adv.jl
@@ -0,0 +1,37 @@
+# Testing divand in 2 dimensions with advection.
+
+# grid of background field
+xi,yi = ndgrid(linspace(-1,1,30),linspace(-1,1,30));
+
+x = .4;
+y = .4;
+f = 1;
+
+mask = trues(xi);
+pm = ones(xi) / (xi[2,1]-xi[1,1]);
+pn = ones(xi) / (yi[1,2]-yi[1,1]);
+
+a = 5;
+u = a*yi;
+v = -a*xi;
+
+fi,s = divandrun(mask,(pm,pn),(xi,yi),(x,y),f,.2,200,velocity = (u,v));
+
+@test abs(fi[18,24] - 0.89935) < 1e-3
+
+  
+
+# Copyright (C) 2014, 2017 Alexander Barth <a.barth@ulg.ac.be>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
