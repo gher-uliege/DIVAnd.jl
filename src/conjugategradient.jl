e13a0004261bcecfd55b17449574f3e8da5b8c31
Alexander Barth <barth.alexander@gmail.com>
2017-02-08 11:57:35 +0100

    improve doc

--- src/conjugategradient.jl
+++ src/conjugategradient.jl
@@ -1,34 +1,45 @@
 
 """
-Solve a linear system with the preconditioned conjugated-gradient method.
+x,success,niter = conjugategradient(fun,b,x0,tol,maxit,pc);
 
-x,success,niter = conjugategradient(fun,b,tol,maxit,pc,x0);
-
-solves for x
+Solve a linear system with the preconditioned conjugated-gradient method:
 A x = b
-using the preconditioned conjugated-gradient method.
-It provides also an approximation of A:
-A \sim Q*T*Q'
+where `A` is a symmetric positive defined matrix and `b` is a vector. The function `fun(x)` represents `A*x`.
 
-J(x) = 1/2 (x' b - x' A x)
-∇ J = b - A x
-A x = b - ∇ J
-b = ∇ J(0)
+# Optional input arguments
+* `x0`: starting vector for the interations
+* `tol`: tolerance on Ax-b
+* `maxit`: maximum of interations
+* `pc`: the preconditioner. The functions `pc(x)` should return M⁻¹ x (the inverse of M times x) where `M` is a symmetric positive defined matrix. Effectively, the system E⁻¹ A (E⁻¹)ᵀ (E x) = E⁻¹ b is solved for (E x) where E Eᵀ = M. Ideally, M should this be similar to A, so that E⁻¹ A (E⁻¹)ᵀ is close to the identity matrix.
 
-the columns of Q are the Lanczos vectors
+# Output
+* `x`: the solution
+* `success`: true if the interation converged (otherwise false)
+* `niter`: the number of iterations
 """
 
 
-function conjugategradient(fun,b; pc = x -> x, tol = 1e-6, maxit = min(size(b,1),20), minit = 0, renorm = false, x0 = zeros(size(b)))
+# It provides also an approximation of A:
+# A \sim Q*T*Q'
+
+# J(x) = 1/2 (x' b - x' A x)
+# ∇ J = b - A x
+# A x = b - ∇ J
+# b = ∇ J(0)
+
+# the columns of Q are the Lanczos vectors
+
+function conjugategradient(fun,b; pc = x -> x, x0 = zeros(size(b)), tol = 1e-6, maxit = min(size(b,1),20), minit = 0)
+#, renorm = false)
 
 success = false
 n = length(b);
 
 tol2 = tol^2;
 
-delta = [];
-gamma = [];
-q = NaN*zeros(n,1);
+# delta = [];
+# gamma = [];
+# q = NaN*zeros(n,1);
 
 #M = inv(invM);
 #E = chol(M);
@@ -70,38 +81,37 @@ for k=1:maxit
     #     # keep at most n vectors
     #     Q(:,k) = r/sqrt(zr_old);
     # end
-    
+
     # compute A*p
     Ap = fun(p);
-    
+
     # how far do we need to go in direction p?
     # alpha is determined by linesearch
 
     alpha[k] = zr_old / (p ⋅ Ap);
-    
+
     # get new estimate of x
     x = x + alpha[k]*p;
-    
+
     # recompute gradient at new x. Could be done by
     # r = b-fun(x);
     # but this does require an new call to fun
     r = r - alpha[k]*Ap;
-    
-    if renorm
-        r = r - Q(:,1:k) * Q(:,1:k)' * r ;
-    end
-    #maxdiff(r,b-fun(x))
-    
+
+    #if renorm
+    #    r = r - Q(:,1:k) * Q(:,1:k)' * r ;
+    #end
+
     # apply pre-conditionner
     z = pc(r);
-        
+
     zr_new = r ⋅ z;
-        
+
     if r ⋅ r < tol2 && k >= minit
         success = true
         break
     end
-    
+
     #Fletcher-Reeves
     beta[k+1] = zr_new / zr_old;
     #Polak-Ribiere
@@ -109,8 +119,8 @@ for k=1:maxit
     #Hestenes-Stiefel
     #beta[k+1] = r'*(r-r_old) / (p'*(r-r_old));
     #beta[k+1] = r'*(r-r_old) / (r_old'*r_old);
-    
-    
+
+
     # norm(p)
     p = z + beta[k+1]*p;
     zr_old = zr_new;
@@ -126,23 +136,23 @@ return x,success,k
 
 # if nargout > 1
 #     kmax = size(Q,2);
-    
+
 #     delta[1] = 1/alpha[1];
-    
+
 #     #delta[1] - Q[:,1]'*invM*A*invM*Q[:,1]
-    
-    
+
+
 #     for k=1:kmax-1
 #         delta[k+1] = 1/alpha[k+1] + beta[k+1]/alpha[k];
 #         gamma[k] = -sqrt(beta[k+1])/alpha[k];
 #     end
-    
+
 #     T = sparse([1:kmax   1:kmax-1 2:kmax  ],...
 #         [1:kmax   2:kmax   1:kmax-1],...
 #         [delta    gamma    gamma]);
-    
+
 #     diag.iter = k;
-#     diag.relres = sqrt(r'*r); 
+#     diag.relres = sqrt(r'*r);
 # end
 
 end
