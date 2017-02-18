662458c52760d5da9f49d0fb0f94e8d0f72c6de7
Alexander Barth <barth.alexander@gmail.com>
2017-02-18 15:32:55 +0100

    sparse_diff adjoint for nD (clean-up)

--- src/function_operator.jl
+++ src/function_operator.jl
@@ -39,63 +39,8 @@ Input:
 
 function matfun_diff(sz1,m,cyclic = false)
 
-n1 = prod(sz1)
-
-# make a copy and convert tuple to array
-sz2 = collect(sz1)
-
-if !cyclic
-    sz2[m] = sz2[m]-1
-end
-
-n2 = prod(sz2)
-n2i = Int(n2)
-n = length(sz1)
-
-
-strides1 = [1,cumprod(collect(sz1))[1:end-1]...]
-
-L2 = Array{Int64,1}(n2)
-
-
-# L2 = [i for i = 1:n1 if ind2sub(sz1,i)[m] != sz1[m] ]
-# L2o = L2 + strides1[m]
-
-
-vi = [collect(1:sz2[i]) for i = 1:n]
-#IJ = [_[:] for _ in ndgrid(vi...)]
-
-#IJ = [Array{Int64,1}(n2) for i = 1:n]
-IJ = [zeros(Int64,Int(n2)) for i = 1:n]
-
-    s = 1
-    for i=1:n
-        v = vi[i]
-        snext = s*sz2[i]
-        ndgrid_fill(IJ[i], v, s, snext)
-        s = snext
-    end
-
-
-
-#L1 = [1:Int(n2);]
-L1 = [1:n2i;]
-L2 = sub2ind(sz1,IJ...)::Array{Int64,1}
-one = ones(size(L1))
-
-IJ[m] = IJ[m] + 1
-
-if cyclic
-     IJ[m] = mod(IJ[m]-1,sz1[m])+1
-end
-
-L2o = sub2ind(sz1,IJ...)::Array{Int64,1}
-
-S = sparse(
-    [L1;     L1;  ],
-    [L2;     L2o; ],
-    [-one;   one  ], n2 , n1 )
-
+# sz2 size of the resulting array
+sz2 = ntuple(i -> (i == m && !cyclic ? sz1[i]-1 : sz1[i]), length(sz1))
 
 function fun(x)
     x = reshape(x,sz1)
@@ -110,8 +55,9 @@ function fun(x)
     end
 end
 
+# adjoint
 function funt(x)
-    x2 = reshape(x,(sz2...))
+    x2 = reshape(x,sz2)
     @show "here"
 
     if !cyclic
@@ -127,7 +73,7 @@ function funt(x)
         return (-x2 + x2[ind...])[:]
     end
 end
-return MatFun((n2,n1),fun,funt)
+return MatFun((prod(sz2),prod(sz1)),fun,funt)
 
 end
 
