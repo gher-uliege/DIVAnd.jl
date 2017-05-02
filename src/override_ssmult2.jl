39fe101b857d22ccce445766caf7fbfcdd747a01
Alexander Barth <barth.alexander@gmail.com>
2017-05-02 11:54:50 +0200

    test swap and transpose

---
+++ src/override_ssmult2.jl
@@ -0,0 +1,139 @@
+# adapted from Julia
+
+
+"""compute non-zero values of A*B"""
+function myspmatmul_nnz{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, B::SparseMatrixCSC{Tv,Ti})
+
+    mA, nA = size(A)
+    mB, nB = size(B)
+    nA==mB || throw(DimensionMismatch())
+
+    colptrA = A.colptr; rowvalA = A.rowval; nzvalA = A.nzval
+    colptrB = B.colptr; rowvalB = B.rowval; nzvalB = B.nzval
+
+    ip = 1::Ti
+
+    @inbounds begin
+        xb = zeros(Ti, mA)
+        for i in 1:nB
+
+            for jp in colptrB[i]:(colptrB[i+1] - 1)
+
+                j = rowvalB[jp]
+                for kp in colptrA[j]:(colptrA[j+1] - 1)
+                    k = rowvalA[kp]
+                    if xb[k] != i
+                        ip += 1
+                        xb[k] = i
+                    end
+                end
+            end
+        end
+    end
+
+    return ip-1
+end
+
+
+
+function myspmatmul_unsorted{Tv,Ti}(A::SparseMatrixCSC{Tv,Ti}, B::SparseMatrixCSC{Tv,Ti}, nnzC::Ti)
+
+    mA, nA = size(A)
+    mB, nB = size(B)
+    nA==mB || throw(DimensionMismatch())
+
+    colptrA = A.colptr; rowvalA = A.rowval; nzvalA = A.nzval
+    colptrB = B.colptr; rowvalB = B.rowval; nzvalB = B.nzval
+
+    colptrC = Array{Ti}(nB+1)
+    rowvalC = Array{Ti}(nnzC)
+    nzvalC = Array{Tv}(nnzC)
+
+    @inbounds begin
+        ip = 1
+        xb = zeros(Ti, mA)
+        x  = zeros(Tv, mA)
+        for i in 1:nB
+            colptrC[i] = ip
+            for jp in colptrB[i]:(colptrB[i+1] - 1)
+                nzB = nzvalB[jp]
+                j = rowvalB[jp]
+                for kp in colptrA[j]:(colptrA[j+1] - 1)
+                    nzC = nzvalA[kp] * nzB
+                    k = rowvalA[kp]
+                    if xb[k] != i
+                        rowvalC[ip] = k
+                        ip += 1
+                        xb[k] = i
+                        x[k] = nzC
+                    else
+                        x[k] += nzC
+                    end
+                end
+            end
+            for vp in colptrC[i]:(ip - 1)
+                nzvalC[vp] = x[rowvalC[vp]]
+            end
+        end
+        colptrC[nB+1] = ip
+    end
+
+    Cunsorted = SparseMatrixCSC(mA, nB, colptrC, rowvalC, nzvalC)
+    return Cunsorted
+end
+
+
+
+function Base.:*(A::SparseMatrixCSC{Float64,Int64},B::SparseMatrixCSC{Float64,Int64})
+
+    nnzC = myspmatmul_nnz(A,B)
+    #@show nnz(A),nnz(B),nnzC
+    if nnz(A) + nnz(B) < nnzC
+        # compute A*B as (B'*A')'
+        # swap and transpose        
+        #@show "swap"
+        return myspmatmul_unsorted(B',A',nnzC)'        
+    else
+        Cunsorted = myspmatmul_unsorted(A,B,nnzC)
+        C = SparseArrays.sortSparseMatrixCSC!(Cunsorted, sortindices=:doubletranspose)
+        return C
+    end
+end
+
+
+function Base.:A_mul_Bc(A::SparseMatrixCSC{Float64,Int64},B::SparseMatrixCSC{Float64,Int64})
+
+    BT = B'
+    nnzC = myspmatmul_nnz(A,BT)
+    #@show nnz(A),nnz(B),nnzC
+    if nnz(A) + nnz(B) < nnzC
+        # compute A*B' as (B*A')'
+        # swap and transpose        
+        #@show "swap"
+        return myspmatmul_unsorted(B,A',nnzC)'        
+    else
+        Cunsorted = myspmatmul_unsorted(A,BT,nnzC)
+        C = SparseArrays.sortSparseMatrixCSC!(Cunsorted, sortindices=:doubletranspose)
+        return C
+    end
+end
+
+
+function Base.:Ac_mul_B(A::SparseMatrixCSC{Float64,Int64},B::SparseMatrixCSC{Float64,Int64})
+
+    AT = A'
+    nnzC = myspmatmul_nnz(AT,B)
+    #@show nnz(A),nnz(B),nnzC
+    if nnz(A) + nnz(B) < nnzC
+        # compute A'*B as (B'*A)'
+        # swap and transpose        
+        #@show "swap"
+        return myspmatmul_unsorted(B',A,nnzC)'        
+    else
+        Cunsorted = myspmatmul_unsorted(AT,B,nnzC)
+        C = SparseArrays.sortSparseMatrixCSC!(Cunsorted, sortindices=:doubletranspose)
+        return C
+    end
+end
+
+
