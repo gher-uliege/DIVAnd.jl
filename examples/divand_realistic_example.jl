f318ff2a103879df3668d5cf90f4704ddc72fbe2
Alexander Barth <barth.alexander@gmail.com>
2017-06-11 22:40:30 +0200

    call high-level diva function

--- examples/divand_realistic_example.jl
+++ examples/divand_realistic_example.jl
@@ -1,103 +1,58 @@
 using divand
 using NetCDF
 
-function loadbigfile(fname)
-
-    data = readlines(open(fname,"r"))
-    nobs = length(data)
-
-    lon = zeros(nobs)
-    lat = zeros(nobs)
-    depth = zeros(nobs)
-    time = Array{DateTime}(nobs)
-    value = zeros(nobs)
-    id = Array{String}(nobs)
+fname = joinpath(dirname(@__FILE__),"..","..","divand-example-data","BlackSea","Salinity.bigfile")
+bathname = joinpath(dirname(@__FILE__),"..","..","divand-example-data","Global","Bathymetry","gebco_30sec_16.nc")
 
 
-    for i in 1:nobs
-        rec = split(data[i])
-        lon[i] = parse(Float64,rec[1])
-        lat[i] = parse(Float64,rec[2])
-        value[i] = parse(Float64,rec[3])
-        depth[i] = parse(Float64,rec[4])
-        time[i] = DateTime(rec[10])
-        id[i] = rec[11]
-    end
-
-    return value,lon,lat,depth,time,id
-end
-
-include("../src/override_ssmult.jl")
-
-datadir = joinpath(dirname(@__FILE__),"..","..","divand-example-data")
-fname = joinpath(datadir,"BlackSea","Salinity.bigfile")
-bathname = joinpath(datadir,"Global","Bathymetry","gebco_30sec_16.nc")
-isglobal = true
-
-if !isdefined(:value)
-    value,lon,lat,depth,time,id = loadbigfile(fname)
-end
+obsvalue,obslon,obslat,obsdepth,obstime,obsid = loadbigfile(fname)
 
 dx = dy = 0.1
-dx = dy = 0.25
 lonr = 27:dx:42
 latr = 40:dy:47
-#depthr = [0., 10, 20, 30, 50, 75, 100, 125, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1750, 2000];
+depthr = [0., 10, 20, 30, 50, 75, 100, 125, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1750, 2000];
 #depthr = [0, 10, 20, 30, 50, 75, 100];
-depthr = 0:10.:30.;
-
+#depthr = 0:10.:30.;
+depthr = [0.]
 
 timer = 1:1.:12
+timer = [1]
 
-mask,(pm,pn,po,pp),(xi,yi,zi,ti) = divand_rectdom(lonr,latr,depthr,timer)
 
+# signal-to-noise ratio
 epsilon2 = 0.1
 
-time2 = Dates.month.(time)
+# size of the domain
 
-mxi,myi,mask2 = load_mask(bathname,isglobal,lonr,latr,depthr)
-mask3 = repeat(mask2,inner = (1,1,1,length(timer)))
+sz = (length(lonr),length(latr),length(depthr),length(timer))
 
+# horizontal correlation length in meters
+lenx = 100_000 # m
+leny = 100_000 # m
 
-sz = size(mask)
+# vertical correlation length in meters
+lenz = Array{Float64}(sz)
+for n = 1:sz[4]
+    for k = 1:sz[3]
+        for j = 1:sz[2]
+            for i = 1:sz[1]
+                lenz[i,j,k,n] = 10 + depthr[k]/5
+            end
+        end
+    end
+end
 
-z = zeros(sz)
-# correlation length in arc degree
-lenx = fill(1.,sz)
-leny = fill(1.,sz)
-# correlation length in meters
-lenz = 10 + zi/5
 # correlation time-scale in month
-lent = fill(1.,sz)
+lent = 1. # month
 
-vm = mean(value)
-va = value - vm
-@time fi,s = divandrun(mask3,(pm,pn,po,pp),(xi,yi,zi,ti),(lon,lat,depth,time2),va,(lenx,leny,z,lent),epsilon2)
-fi = fi + vm;
+@time fi = diva(("longitude","latitude","depth","time"),
+          (lonr,latr,depthr,timer),
+          (obslon,obslat,obsdepth,obstime),
+          obsvalue, epsilon2,
+          (lenx,leny,lenz,lent),
+          divand.aggregation_monthly;
+          bathname = bathname
+          )
 
-
-#@time fip,sp = divandrun(mask3,(pm,pn,po,pp),(xi,yi,zi,ti),(lon,lat,depth,time2),va,(1,1,0,0.00001),epsilon2)
-#fip = fip+vm;
-
-# tolerance on the gradient A x - b
-tol = 1e-4
-# tolerance on the result x
-tolres = 1e-3
-
-kwargs = [(:tol, tol),(:maxit,10000),(:minit,0)]
-
-
-compPC(iB,H,R) = x -> s.P*x
-
-@time fi2,s = divandrun(mask3,(pm,pn,po,pp),(xi,yi,zi,ti),(lon,lat,depth,time2),va,(lenx,leny,lenz,lent),epsilon2;
-                        kwargs...,inversion=:pcg,operatortype=Val{:MatFun},fi0=fi
-                        ,compPC = compPC
-                        )
-fi2 = fi2+vm;
-
-@show s.niter
-
-
-divand_save(replace(@__FILE__,r".jl$",".nc"),mask,"salinity",fi2)
-
+#divand_save(replace(@__FILE__,r".jl$",".nc"),mask,"salinity",fi2)
 nothing
