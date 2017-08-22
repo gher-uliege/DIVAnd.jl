c953759d699f26af853cfdc0f06dc7b5c376d4bd
Alexander Barth <barth.alexander@gmail.com>
2017-08-22 11:17:55 +0200

    add example

--- test/perf/run_benchmark.sh
+++ test/perf/run_benchmark.sh
@@ -7,7 +7,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=18000
 
-# run as run_benchmark.sh benchmark.jl
+# run as:
+# sbatch run_benchmark.sh benchmark1.jl 4 varanalysis
+# for i in $(seq 2 4); do sbatch run_benchmark.sh benchmark1.jl $i varanalysis; done
 
 module load  EasyBuild  Python/3.5.1-foss-2016a
 srun --mem-per-cpu=16000 --ntasks=1 --time=10:20:00 julia "$@"
