e09b83994ee424b62ae72f5ee5e53bd3480cb8fb
Alexander Barth <barth.alexander@gmail.com>
2018-02-21 21:48:41 +0100

    optimize vertchoose

--- examples/example_vertcorrlen.jl
+++ examples/example_vertcorrlen.jl
@@ -21,7 +21,9 @@ function vertcorrlen(x,value::Vector{T},z;
   end
 
   zlevel2 = zero(T)
-
+    zindex = Vector{Int}(length(value))
+    nzindex = 0
+        
   function pickone(mask)
     ii = find(mask)
     jindex = rand(1:length(ii))
@@ -31,15 +33,27 @@ function vertcorrlen(x,value::Vector{T},z;
 
 
   function vertchoose(x,zlevel)
-    mask = abs.(zlevel - x[3]) .< 20
-    j = pickone(mask);
+    #mask = abs.(zlevel - x[3]) .< 20
+    #j = pickone(mask);
 
-    mask = falses(size(x[1]))
-    for k = 1:length(x[1])
-      mask[k] = distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
-    end
+      
+    jindex = rand(1:length(zindex))
+    j = zindex[jindex] :: Int
 
-    jindex = pickone(mask)
+    #mask = falses(size(x[1]))
+    #for k = 1:length(x[1])
+    #  mask[k] = distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
+      #end
+    #jindex = pickone(mask)
+      jindex = -1
+      
+      while true
+          k = rand(1:length(x[1]))
+          if distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
+              jindex = k
+              break
+          end
+      end
 
     #@show j,jindex
     return j,jindex
@@ -99,6 +113,8 @@ function vertcorrlen(x,value::Vector{T},z;
 
   for k = 1:length(z)
     zlevel2 = Float64(z[k])
+    zindex = find(abs.(zlevel2 - x[3]) .< 20)
+      
     @show zlevel2,maxpoints,nmean
 
     var0[k],len[k],distx[:],covar[:,k],fitcovar[:,k] = divand.fit_isotropic(
@@ -134,7 +150,24 @@ sel = Dates.month.(time) .== 1;
 x = (lon[sel],lat[sel],depth[sel]);
 v = value[sel]
 z = [0.,10,100,200,300,400,500,700,1000,1500]
-#z = [0.,10,100]
+z = [0.,10]
 
 
-lenz,dbinfo = vertcorrlen(x,v,z)
+srand(12345);
+@time lenz,dbinfo = vertcorrlen(x,v,z)
+
+srand(12345);
+@time lenz,dbinfo = vertcorrlen(x,v,z)
+
+
+#=
+srand(12345); @time lenz,dbinfo = vertcorrlen(x,v,z)
+
+Data points at z=0.0: 3065, correlation length: 15.31864937639221
+(zlevel2, maxpoints, nmean) = (10.0, 10000, 10)
+Data points at z=10.0: 3065, correlation length: 127.22510065919151
+184.761368 seconds (4.73 G allocations: 230.815 GiB, 25.09% gc time)
+([71.2719, 71.2719], Dict{Symbol,Any}(Pair{Symbol,Any}(:fitcovar, [2.31656 0.126384; 0.0628743 0.105038; … ; 4.74498e-31 0.000139351; 6.87451e-35 5.24698e-5]),Pair{Symbol,Any}(:covar, [2.48498 1.59739; 0.0538359 0.047323; … ; 0.00147103 0.00147502; 0.00126514 0.00189619]),Pair{Symbol,Any}(:len, [15.3186, 127.225]),Pair{Symbol,Any}(:distx, [25.0, 75.0, 125.0, 175.0, 225.0, 275.0, 325.0, 375.0, 450.0, 550.0, 650.0, 750.0, 850.0, 950.0]),Pair{Symbol,Any}(:var0, [6.70391, 0.130344])))
+
+
+=#
