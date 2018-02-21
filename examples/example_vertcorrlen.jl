a19d6062f45e2b875cb930a28d43214ff141140f
Alexander Barth <barth.alexander@gmail.com>
2018-02-21 22:04:00 +0100

    indentation

--- examples/example_vertcorrlen.jl
+++ examples/example_vertcorrlen.jl
@@ -1,4 +1,4 @@
-using Base.Test
+    using Base.Test
 import divand
 using PyPlot
 
@@ -7,151 +7,151 @@ fname = joinpath(dirname(@__FILE__),"..","..","divand-example-data","BlackSea","
 
 
 function vertcorrlen(x,value::Vector{T},z;
-  distbin = collect([0.:50:400; 500:100:1000]),
-  mincount = 50,
-  maxpoints = 10000,
-  nmean = 10,
-  len0 = 50.,
-  maxlen = 1000.,
-  smoothz = 100.,
-  ) where T
+                     distbin = collect([0.:50:400; 500:100:1000]),
+                     mincount = 50,
+                     maxpoints = 10000,
+                     nmean = 10,
+                     len0 = 50.,
+                     maxlen = 1000.,
+                     smoothz = 100.,
+                     ) where T
 
-  function distfun(xi,xj)
-    sqrt(sum(abs2,xi-xj))
-  end
-
-  zlevel2 = zero(T)
-    zindex = Vector{Int}(length(value))
-    nzindex = 0
-        
-  function pickone(mask)
-    ii = find(mask)
-    jindex = rand(1:length(ii))
-    return ii[jindex]
-  end
-
-
-
-  function vertchoose(x,zlevel)
-    #mask = abs.(zlevel - x[3]) .< 20
-      #j = pickone(mask);
-      maxntries = 10000
-      maxntries2 = 1000
-      j = -1
-      jindex = -1
-      
-    for ntries = 1:maxntries  
-      
-    j = zindex[rand(1:length(zindex))] :: Int
-
-    #mask = falses(size(x[1]))
-    #for k = 1:length(x[1])
-    #  mask[k] = distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
-      #end
-    #jindex = pickone(mask)
-      jindex = -1
-
-        for ntries2 = 1:maxntries2
-          k = rand(1:length(x[1]))
-          if distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
-              jindex = k
-              break
-          end
-        end
-
-        if jindex != -1
-            break
-        end
+    function distfun(xi,xj)
+        sqrt(sum(abs2,xi-xj))
     end
 
-      if (j == -1) || (jindex == -1)
-          error("fail to find enought pairs at z = $(zlevels2)")
-      end
-    #@show j,jindex
-    return j,jindex
-  end
+    zlevel2 = zero(T)
+    zindex = Vector{Int}(length(value))
+    nzindex = 0
+    
+    function pickone(mask)
+        ii = find(mask)
+        jindex = rand(1:length(ii))
+        return ii[jindex]
+    end
 
 
-  function vchoose(dummy)
-    vertchoose(x,zlevel2::Float64)
-  end
 
-  #@code_warntype vertchoose(x,zlevel2)
-  #@code_warntype vchoose(x)
+    function vertchoose(x,zlevel)
+        #mask = abs.(zlevel - x[3]) .< 20
+        #j = pickone(mask);
+        maxntries = 10000
+        maxntries2 = 1000
+        j = -1
+        jindex = -1
+        
+        for ntries = 1:maxntries  
+            
+            j = zindex[rand(1:length(zindex))] :: Int
 
-  pmax = length(distbin)-1
-  kmax = length(z)
-  len = zeros(kmax)
-  var0 = zeros(kmax)
-  fitcovar = Array{T,2}(pmax,kmax)
-  covar = Array{T,2}(pmax,kmax)
-  distx = Vector{T}(pmax)
+            #mask = falses(size(x[1]))
+            #for k = 1:length(x[1])
+            #  mask[k] = distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
+            #end
+            #jindex = pickone(mask)
+            jindex = -1
 
+            for ntries2 = 1:maxntries2
+                k = rand(1:length(x[1]))
+                if distfun([x[1][k],x[2][k]],[x[1][j],x[2][j]]) < 0.5
+                    jindex = k
+                    break
+                end
+            end
 
-  # @time distx,covar,corr,varx,count = divand.empiriccovarmean(
-  #      (depth[sel],),value[sel],distbin,mincount;
-  #      maxpoints = maxpoints,
-  #      nmean = nmean,
-  #      choose = vchoose,
-  #      distfun = (xi,xj) -> sqrt(sum(abs2,xi-xj)))
+            if jindex != -1
+                break
+            end
+        end
 
-
-  # k = 1
-  # @time var0[k],len[k],distx,covar,fitcovar = divand.fit_isotropic(
-  #      (depth[sel],),value[sel],distbin,mincount;
-  #      maxpoints = maxpoints,
-  #      nmean = nmean,
-  #      maxlen = maxlen,
-  #      choose = vchoose)
-
-
-  # clf()
-  #
-  # k = 10
-  # distx,covar,corr,varx,count = divand.empiriccovarmean(
-  #     (depth[sel],),value[sel],distbin,mincount;
-  #     maxpoints = maxpoints,
-  #     nmean = nmean,
-  #     choose = vchoose)
-  #
-  #     @time var0[k],len[k],distx,covar,fitcovar = divand.fit_isotropic(
-  #         (depth[sel],),value[sel],distbin,mincount;
-  #         maxpoints = maxpoints,
-  #         nmean = nmean,
-  #         maxlen = maxlen,
-  #         len = len0,
-  #         choose = vchoose)
+        if (j == -1) || (jindex == -1)
+            error("fail to find enought pairs at z = $(zlevels2)")
+        end
+        #@show j,jindex
+        return j,jindex
+    end
 
 
-  for k = 1:length(z)
-    zlevel2 = Float64(z[k])
-    zindex = find(abs.(zlevel2 - x[3]) .< 20)
-      
-    @show zlevel2,maxpoints,nmean
+    function vchoose(dummy)
+        vertchoose(x,zlevel2::Float64)
+    end
 
-    var0[k],len[k],distx[:],covar[:,k],fitcovar[:,k] = divand.fit_isotropic(
-    (x[3],),value,distbin,mincount;
-    maxpoints = maxpoints,
-    nmean = nmean,
-    maxlen = maxlen,
-    len = len0,
-    choose = vchoose)
+    #@code_warntype vertchoose(x,zlevel2)
+    #@code_warntype vchoose(x)
 
-     println("Data points at z=$(z[k]): $(length(v)), correlation length: $(len[k])")
-    #plot(distx,covar, label = "empirical covariance")
-    #plot(distx,fitcovar, label = "fitted function")
-  end
-  lenf = copy(len)
-  if smoothz > 0
-    divand.smoothfilter!(z,lenf,smoothz)
-  end
+    pmax = length(distbin)-1
+    kmax = length(z)
+    len = zeros(kmax)
+    var0 = zeros(kmax)
+    fitcovar = Array{T,2}(pmax,kmax)
+    covar = Array{T,2}(pmax,kmax)
+    distx = Vector{T}(pmax)
 
-  return lenf,Dict(
-  :var0 => var0,
-  :len => len,
-  :distx => distx,
-  :covar => covar,
-  :fitcovar => fitcovar)
+
+    # @time distx,covar,corr,varx,count = divand.empiriccovarmean(
+    #      (depth[sel],),value[sel],distbin,mincount;
+    #      maxpoints = maxpoints,
+    #      nmean = nmean,
+    #      choose = vchoose,
+    #      distfun = (xi,xj) -> sqrt(sum(abs2,xi-xj)))
+
+
+    # k = 1
+    # @time var0[k],len[k],distx,covar,fitcovar = divand.fit_isotropic(
+    #      (depth[sel],),value[sel],distbin,mincount;
+    #      maxpoints = maxpoints,
+    #      nmean = nmean,
+    #      maxlen = maxlen,
+    #      choose = vchoose)
+
+
+    # clf()
+    #
+    # k = 10
+    # distx,covar,corr,varx,count = divand.empiriccovarmean(
+    #     (depth[sel],),value[sel],distbin,mincount;
+    #     maxpoints = maxpoints,
+    #     nmean = nmean,
+    #     choose = vchoose)
+    #
+    #     @time var0[k],len[k],distx,covar,fitcovar = divand.fit_isotropic(
+    #         (depth[sel],),value[sel],distbin,mincount;
+    #         maxpoints = maxpoints,
+    #         nmean = nmean,
+    #         maxlen = maxlen,
+    #         len = len0,
+    #         choose = vchoose)
+
+
+    for k = 1:length(z)
+        zlevel2 = Float64(z[k])
+        zindex = find(abs.(zlevel2 - x[3]) .< 20)
+        
+        @show zlevel2,maxpoints,nmean
+
+        var0[k],len[k],distx[:],covar[:,k],fitcovar[:,k] = divand.fit_isotropic(
+            (x[3],),value,distbin,mincount;
+            maxpoints = maxpoints,
+            nmean = nmean,
+            maxlen = maxlen,
+            len = len0,
+            choose = vchoose)
+
+        println("Data points at z=$(z[k]): $(length(v)), correlation length: $(len[k])")
+        #plot(distx,covar, label = "empirical covariance")
+        #plot(distx,fitcovar, label = "fitted function")
+    end
+    lenf = copy(len)
+    if smoothz > 0
+        divand.smoothfilter!(z,lenf,smoothz)
+    end
+
+    return lenf,Dict(
+        :var0 => var0,
+        :len => len,
+        :distx => distx,
+        :covar => covar,
+        :fitcovar => fitcovar)
 
 end
 
