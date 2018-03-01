29810388787918e344b112aa0a0a3f094f8e7d06
ctroupin <charles.troupin@gmail.com>
2018-03-01 15:11:54 +0100

    create output and figure directories

--- examples/prep_dirs.jl
+++ examples/prep_dirs.jl
@@ -1,3 +1,10 @@
+#=
+prep_dirs.jl
+
+Create figure and netCDF directories to store the results of the examples
+Can also add other setup operations needed for to run the examples.
+=#
+
 figdir = "./figures/"
 outputdir = "./netCDF/"
 logging
