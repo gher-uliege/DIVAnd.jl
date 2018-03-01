744f2b639da612df901d871814c1ffd012700f15
Alexander Barth <barth.alexander@gmail.com>
2018-03-01 09:03:45 +0100

    download bathymetry if necessary

--- test/test_domain.jl
+++ test/test_domain.jl
@@ -3,6 +3,10 @@ import divand
 bathname = joinpath(dirname(@__FILE__),"..","..","divand-example-data","Global","Bathymetry","gebco_30sec_16.nc")
 bathisglobal = true
 
+if !isfile(bathname)
+    bathname = "gebco_30sec_16.nc"
+    download("https://b2drop.eudat.eu/s/o0vinoQutAC7eb0/download",bathname)
+end
 
 dx = dy = 0.1
 dx = dy = 0.2
