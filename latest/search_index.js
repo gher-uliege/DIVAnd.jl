a2c225da1fc86f9779d4dbb6867848af4fd89774
autodocs <autodocs>
2018-03-06 13:59:11 +0000

    build based on cc48034

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "divand.jl documentation",
     "title": "divand.jl documentation",
     "category": "section",
-    "text": "divand.divandrun\ndivand.divandgo\ndivand.divand_averaged_bg\ndivand.load_mask\ndivand.domain\ndivand.SDNMetadata\ndivand.save\ndivand.saveobs\ndivand.loadobs\ndivand.loadbigfile\ndivand.checkobs\ndivand.fit_isotropic\ndivand.fit\ndivand.smoothfilter\ndivand.Anam.loglin\ndivand.Anam.logit\ndivand.divadoxml\ndivand.random\ndivand.distance\ndivand.metric"
+    "text": "divand.divandrun\ndivand.divandgo\ndivand.divand_averaged_bg\ndivand.load_mask\ndivand.domain\ndivand.SDNMetadata\ndivand.save\ndivand.saveobs\ndivand.loadobs\ndivand.loadbigfile\ndivand.checkobs\ndivand.fit_isotropic\ndivand.fit\ndivand.smoothfilter\ndivand.Anam.loglin\ndivand.Anam.logit\ndivand.divadoxml\ndivand.random\ndivand.distance\ndivand.metric\ndivand.interp\ndivand.backgroundfile"
 },
 
 {
@@ -28,7 +28,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.CFVocab",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.CFVocab",
-    "category": "Type",
+    "category": "type",
     "text": "collection = Vocab.CFVocab(url)\n\nCreate a Dict-like object represeting the NetCDF CF Standard Name vocabulary. If the url is not provided then current CF Standard Name list http://cfconventions.org/Data/cf-standard-names/current/src/cf-standard-name-table.xml is used. Individual standard names are retirved by indexing which return an object of the type CFEntry:\n\ncollection = Vocab.CFVocab()\nentry = collection[\"sea_water_temperature\"]\n\n\n\n"
 },
 
@@ -36,7 +36,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#Base.haskey-Tuple{divand.Vocab.CFVocab,Any}",
     "page": "divand.jl documentation",
     "title": "Base.haskey",
-    "category": "Method",
+    "category": "method",
     "text": "bool = haskey(collection::CFVocab,stdname)\n\nReturn true if stdname is part of the NetCDF CF Standard Name vocabulary collection.\n\n\n\n"
 },
 
@@ -44,7 +44,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.SDNCollection",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.SDNCollection",
-    "category": "Function",
+    "category": "function",
     "text": "collection = SDNCollection(name)\n\nOpen the SeaDataNet collection with the name name at the URL http://www.seadatanet.org/urnurl/collection/ The collection can be indexed with brackets using the identifier.\n\nusing divand\ncollection = Vocab.SDNCollection(\"P01\")\nconcept = collection[\"PSALPR01\"]\n@show Vocab.prefLabel(concept)\n\n\n\n"
 },
 
@@ -52,7 +52,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.prefLabel",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.prefLabel",
-    "category": "Function",
+    "category": "function",
     "text": "s = Vocab.prefLabel(c::Vocab.Concept)\n\nReturn the preferred label of a concept c\n\n\n\ns = Vocab.prefLabel(urn::AbstractString)\n\nReturn the preferred label of a concept usings it URN (Uniform Resource Name)\n\n\n\n"
 },
 
@@ -60,7 +60,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.altLabel",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.altLabel",
-    "category": "Function",
+    "category": "function",
     "text": "s = Vocab.altLabel(c::Vocab.Concept)\n\nReturn the alternative label of a concept c\n\n\n\ns = Vocab.altLabel(urn::AbstractString)\n\nReturn the alternative label of a concept usings it URN (Uniform Resource Name)\n\n\n\n"
 },
 
@@ -68,7 +68,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.notation",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.notation",
-    "category": "Function",
+    "category": "function",
     "text": "s = Vocab.notation(c::Vocab.Concept)\n\nReturn the identifier of a concept c\n\n\n\ns = Vocab.notation(urn::AbstractString)\n\nReturn the identifier of a concept usings it URN (Uniform Resource Name)\n\n\n\n"
 },
 
@@ -76,7 +76,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#Base.find-Tuple{divand.Vocab.Concept,Any,Any}",
     "page": "divand.jl documentation",
     "title": "Base.find",
-    "category": "Method",
+    "category": "method",
     "text": "find(c::Concept,name,collection)\n\nReturn a list of related concepts in the collection collection. name can be the string \"related\", \"narrower\", \"broader\".\n\n\n\n"
 },
 
@@ -84,7 +84,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.description",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.description",
-    "category": "Function",
+    "category": "function",
     "text": "str = description(entry::CFEntry)\nstr = canonical_units(entry::CFEntry)\n\nReturn the description or the canonical units of the `entry`.\n\n\n\n"
 },
 
@@ -92,7 +92,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.canonical_units",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.canonical_units",
-    "category": "Function",
+    "category": "function",
     "text": "str = description(entry::CFEntry)\nstr = canonical_units(entry::CFEntry)\n\nReturn the description or the canonical units of the `entry`.\n\n\n\n"
 },
 
@@ -100,7 +100,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#divand.Vocab.splitURL",
     "page": "divand.jl documentation",
     "title": "divand.Vocab.splitURL",
-    "category": "Function",
+    "category": "function",
     "text": "collection,tag,key = Vocab.splitURL(url)\n\nSplit a concept URL into collection, tag and key. url must finishe with a slash.\n\n\n\n"
 },
 
