c69a40bc768a56d71d61bdeb3b88ac34381cb02f
ctroupin <charles.troupin@gmail.com>
2018-03-08 14:20:07 +0100

    using builtin logging methods

--- src/ODVspreadsheet.jl
+++ src/ODVspreadsheet.jl
@@ -1,11 +1,11 @@
 module ODVspreadsheet
 
-using Logging
+# using Logging
 using StringEncodings
 
 # Set logging level(DEBUG, INFO, WARNING, ERROR or CRITICAL)
-loglevel = WARNING
-Logging.configure(level=loglevel);
+# loglevel = WARNING
+# Logging.configure(level=loglevel);
 
 # SeaDataNet Quality Flags
 # http://vocab.nerc.ac.uk/collection/L20/current/
