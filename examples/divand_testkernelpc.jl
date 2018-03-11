1451432d293c6172b408a274b621341b0fb8c7af
ctroupin <charles.troupin@gmail.com>
2018-03-11 22:28:38 +0100

    tested kernel

--- examples/divand_testkernelpc.jl
+++ examples/divand_testkernelpc.jl
@@ -93,9 +93,7 @@ var(fi-fiiter)/var(fi)
 
 var(fi-fipc2)/var(fi)
 
-
-
-# Copyright (C) 2014, 2017 Alexander Barth <a.barth@ulg.ac.be>
+# Copyright (C) 2014, 2018 Alexander Barth <a.barth@ulg.ac.be>
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
