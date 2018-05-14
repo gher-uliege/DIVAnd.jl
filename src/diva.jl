b47bba0e6cb47f858ef1557728902f1de0865a92
ctroupin <charles.troupin@gmail.com>
2018-05-14 12:51:56 +0200

    add logging in diva3d

--- src/diva.jl
+++ src/diva.jl
@@ -2,8 +2,8 @@
     residuals = diva3d(xi,x,value,len,epsilon2,filename,varname)
 
 Create a 3D analysis (or a series of 3D analyses) with DIVAnd using the
-observations `value` (vector) at the locations `x` (tuple of vector) onto
-the regular grid defined by the vectors `xi` using the scaled obs. error
+observations `value` (vector) at the locations `x` (tuple of vectors) onto
+the regular grid defined by the vectors `xi` using the scaled observational error
 variance  `epsilon2` and the correlation length `len`. The result will be saved in the
 NetCDF file `filename` under the variable `varname`.
 
@@ -18,20 +18,20 @@ NetCDF file `filename` under the variable `varname`.
 * `value`: value of the observations
 
 * `len`: tuple with n elements. Every element represents the correlation length.
-   If `fitcorrlen`, then `len` can be the emply tuple `()` or a tuple containing
-   3 arrays of normalized correlation length which will be multiplied by the
-   horizontal and vertical correlation length.
+   If `fitcorrlen` is `true`, then `len` can be the empty tuple `()` or a tuple containing
+   3 arrays of normalized correlation lengths which will be multiplied by the
+   horizontal and vertical correlation lengths.
 
 * `epsilon2`: error variance of the observations (normalized by the error variance of the background field). `epsilon2` can be a scalar (all observations have the same error variance and their errors are decorrelated), a vector (all observations can have a different error variance and their errors are decorrelated) or a matrix (all observations can have a different error variance and their errors can be correlated). If `epsilon2` is a scalar, it is thus the *inverse of the signal-to-noise ratio*.
 
-* `filename`: The output NetCDF filename
+* `filename`: The output NetCDF filename.
 
-* `varname`: The name of the variable (used in the NetCDF file)
+* `varname`: The name of the variable (used in the NetCDF file).
 
 ## Optional input arguments:
 
 * `bathname`: path to the NetCDF bathymetry (default ../../divand-example-data/Global/Bathymetry/gebco_30sec_16.nc relative to this source file)
-* `bathisglobal`: true (default) is the bahtymetry is a global data set
+* `bathisglobal`: true (default) is the bathymetry is a global data set
 * `plotres`: Call-back routine for plotting ((timeindex,sel,fit,erri) -> nothing)
 * `timeorigin`: Time origin (default DateTime(1900,1,1,0,0,0))
 * `moddim`: modulo for cyclic dimension (vector with n elements).
@@ -39,24 +39,27 @@ NetCDF file `filename` under the variable `varname`.
      not be included for cyclic dimensions. For example if the first dimension
      is cyclic, then the grid point corresponding to `mask[1,j]` should be
      between `mask[end,1]` (left neighbor) and `mask[2,j]` (right neighbor). The default is [0,0,0],
-* `zlevel`: :surface (default) for surface analysis and :floor for analysis from the bottom floor
-* `ncvarattrib`: Dict of NetCDF variable attributes
-* `ncglobalattrib`: Dict of NetCDF global attributes
-* `transform`: Anormphosis transformation function (default: `Anam.notransform()`)
-* `fitcorrlen`: true of the correlation length is determined from the observation (default `false`)
-* `fithorz_param`: is a dictionary with additional optional parameters for `fithorzlen`
-* `fitvert_param`: is a dictionary with additional optional parameters for `fitvertlen`
-* `distfun`: function to compute the distance (default `(xi,xj) -> divand.distance(xi[2],xi[1],xj[2],xj[1])`)
-* `mask`: if different from nothing, then this mask overrride land-sea mask based on the bathymetry (default `nothing`)
-* `background`: if different form, then this parameter allows to load the background from a call-back function (default `nothing`)
-* `background_espilon2_factor`: multiplication for `epsilon2` when computing the background (default 10.)
+* `zlevel`: `:surface` (default) for surface analysis and `:floor` for analysis from the bottom floor.
+* `ncvarattrib`: dictionary of NetCDF variable attributes.
+* `ncglobalattrib`: dictionary of NetCDF global attributes.
+* `transform`: Anamorphosis transformation function (default: `Anam.notransform()`).
+* `fitcorrlen`: true of the correlation length is determined from the observation (default `false`).
+* `fithorz_param`: dictionary with additional optional parameters for `fithorzlen`.
+* `fitvert_param`: dictionary with additional optional parameters for `fitvertlen`.
+* `distfun`: function to compute the distance (default `(xi,xj) -> divand.distance(xi[2],xi[1],xj[2],xj[1])`).
+* `mask`: if different from `nothing`, then this mask overrides land-sea mask based on the bathymetry
+(default `nothing`).
+* `background`: if different from `nothing`, then this parameter allows one
+to load the background from a call-back function
+(default `nothing`).
+* `background_espilon2_factor`: multiplication for `epsilon2` when computing the background (default 10.).
 * `memtofit`: keyword controlling how to cut the domain depending on the memory
-    remaining available for inversion. It is not total memory. (default 3)
-* `niter_e`: Number of uterations to estimate the optimal scale factor of 
+    remaining available for inversion. It is not total memory (default 3).
+* `niter_e`: Number of iterations to estimate the optimal scale factor of
    `epsilon2` using Desroziers et al. 2005 (doi: 10.1256/qj.05.108). The default
     is 1 (i.e. no optimization is done).
 
-Any additional keywoard arguments understood by divandgo can also be used here 
+Any additional keywoard arguments understood by `divandgo` can also be used here
 (e.g. velocity constrain)
 
 """
@@ -148,17 +151,18 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
     len0 = deepcopy((lenx,leny,lenz))
 
 
-    # episilon
-    epsilon2 = 
+    # epsilon
+    epsilon2 =
         if ndims(epsilon2) == 0
             fill(epsilon2,size(value))
         end
-    
+
     # days since timeorigin
     timeclim = [Dates.Millisecond(tc - timeorigin).value/(1000*60*60*24) for tc in ctimes(TS)]
     climatologybounds = climatology_bounds(TS)
 
     # create the NetCDF file
+    info("Creating netCDF file")
     ds, ncvar, ncvar_relerr, ncvar_Lx = divand.ncfile(
         filename,(lonr,latr,depthr,timeclim),varname;
         ncvarattrib = ncvarattrib,
@@ -187,10 +191,15 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
             :distx => zeros(pmax)
         )
     end
-    
+
     dbinfo[:factore] = zeros(niter_e,length(TS))
-    
+
+    info("Starting loop on time indices")
     for timeindex = 1:length(TS)
+
+        info("Step $(timeindex) / $(length(TS))")
+        info("------------")
+
         # select observation to be used for the time instance timeindex
         sel = select(TS,timeindex,time)
 
@@ -216,6 +225,7 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
 
         fbackground,vaa =
             if background == nothing
+                info("Computing background profile")
                 # spatial mean of observations
                 vm = mean(value_trans)
                 va = value_trans - vm
@@ -239,13 +249,14 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
             end
 
         if fitcorrlen
+            info("Applying fit of the correlation length")
             # fit correlation length
             lenxy1,infoxy = divand.fithorzlen(
                 (lon[sel],lat[sel],depth[sel]),vaa,depthr;
                 distfun = distfun,
                 fithorz_param...
             )
-            
+
             lenz1,infoz = divand.fitvertlen(
                 (lon[sel],lat[sel],depth[sel]),vaa,depthr;
                 distfun = distfun,
@@ -259,7 +270,7 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
                 dbinfo[:fitvertlen][key][:,:,timeindex] = infoz[key]
             end
 
-            
+
             # propagate
             for k = 1:size(lenx,3)
                 for j = 1:size(lenx,2)
@@ -281,6 +292,7 @@ function diva3d(xi,x,value,len,epsilon2,filename,varname;
         erri = zeros(sz)
 
         for i = 1:niter_e
+            info("Estimating the optimal scale factor of epsilon2")
             # error only require at the last iterations
             errortype = (i == niter_e ? :cpme : :none)
 
