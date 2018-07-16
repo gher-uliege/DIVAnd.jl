a9b9d39fe794d5a2343b35e96adb31461fdc15c5
Alexander Barth <barth.alexander@gmail.com>
2018-07-16 15:05:54 +0200

    more type stability (2)

--- src/DIVAnd_obscovar.jl
+++ src/DIVAnd_obscovar.jl
@@ -7,7 +7,7 @@ If epsilon2 is a scalar, then R = epsilon2 * I
 If epsilon2 is a vector, then R = diag(epsilon2)
 If epsilon2 is a matrix, then R = epsilon2
 """
-DIVAnd_obscovar(epsilon2::Number,m) = Diagonal([epsilon2 for i=1:m])
+DIVAnd_obscovar(epsilon2::Number,m) = Diagonal(fill(epsilon2,m))
 DIVAnd_obscovar(epsilon2::Vector,m) = Diagonal(epsilon2)
 DIVAnd_obscovar(epsilon2::AbstractMatrix,m) = epsilon2
 
