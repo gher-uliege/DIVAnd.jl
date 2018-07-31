564d7cf4e3132d24533b4e2890cc232c0bd3b2d0
Alexander Barth <barth.alexander@gmail.com>
2018-07-31 14:30:13 +0200

    whitespace formatting

--- src/DIVAnd_fill!.jl
+++ src/DIVAnd_fill!.jl
@@ -1,14 +1,13 @@
 # Adapted from
 # from http://julialang.org/blog/2016/02/iteration
-# to  fill values in a regular grid array. 
+# to  fill values in a regular grid array.
 # The major use to go from a coarse resolution to a fine resolution with grid ratio 2 i
-# It should be quick and better than linear interpolation 
+# It should be quick and better than linear interpolation
 # For the moment just weighted average using the nine points around ...
 #
 
 function DIVAnd_fill!(A::AbstractArray,B::AbstractArray,fillvalue)
 
-    #
     function dvisvalue(x)
         if isnan(fillvalue)
             return !isnan(x);
@@ -16,20 +15,21 @@ function DIVAnd_fill!(A::AbstractArray,B::AbstractArray,fillvalue)
             return !(x==fillvalue);
         end
     end
+
     ntimes=1
     nd=ndims(A)
+
     # central weight
     cw=3^nd-1
     cw=1
-    
 
     R = CartesianRange(size(A))
     I1, Iend = first(R), last(R)
-    for nn=1:ntimes
 
+    for nn=1:ntimes
         for I in R
             w, s = 0.0, zero(eltype(A))
-            
+
             B[I] = A[I]
             if !dvisvalue(A[I])
                 for J in CartesianRange(max(I1, I-3*I1), min(Iend, I+3*I1))
@@ -49,12 +49,9 @@ function DIVAnd_fill!(A::AbstractArray,B::AbstractArray,fillvalue)
                 end
             end
         end
-        
-		
-	A[:]=B[:]	
-		
-    end
 
+	    A[:]=B[:]
+    end
 
     return B
 end
