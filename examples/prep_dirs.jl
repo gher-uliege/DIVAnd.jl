e39089e34d3dff36dc013f505f5be02db3f4a843
Alexander Barth <barth.alexander@gmail.com>
2018-08-02 14:53:16 +0200

    sepatate lines

--- examples/prep_dirs.jl
+++ examples/prep_dirs.jl
@@ -7,5 +7,15 @@ Can also add other setup operations needed for to run the examples.
 
 figdir = "./figures/"
 outputdir = "./netCDF/"
-isdir(figdir) ? info("Directory already exists") : mkdir(figdir);
-isdir(outputdir) ? info("Directory already exists") : mkdir(outputdir);
+
+if isdir(figdir)
+    info("Directory already exists")
+else
+    mkdir(figdir)
+end
+
+if isdir(outputdir)
+    info("Directory already exists")
+else
+    mkdir(outputdir)
+end
