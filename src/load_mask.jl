1cc97af0c2af69d4c9f611260d4429fb87b6df7a
Alexander Barth <barth.alexander@gmail.com>
2018-11-28 14:32:11 +0100

    make mask extration work near the poles

--- src/load_mask.jl
+++ src/load_mask.jl
@@ -14,52 +14,55 @@ function extract_bath(bath_name,isglobal,xi,yi)
 
     #@info "Extracting bathymetry from file: $(bath_name)"
     # if isglobal == true
-        #@info "Working with a global bathymetry";
+    #@info "Working with a global bathymetry";
     # end;
 
     # TODO: make it work for 2-element vector with the bounding box
     dxi = xi[2] - xi[1]
     dyi = yi[2] - yi[1]
 
-    nc = Dataset(bath_name)
-    x = nc["lon"].var[:]
-    y = nc["lat"].var[:]
+    Dataset(bath_name) do nc
+        x = nc["lon"].var[:]
+        y = nc["lat"].var[:]
 
-    rx = x[2]-x[1];
-    ry = y[2]-y[1];
-    X0 = x[1];
-    Y0 = y[1];
-
-    redx = dxi/rx;
-    redy = dyi/ry;
-
-    i0 = round(Int,(xi[1]-dxi-X0)/rx)+1;
-    i1 = round(Int,(xi[end]+dxi-X0)/rx)+1;
-    i=i0:i1;
-
-    j0 = round(Int,(yi[1]-dyi-Y0)/ry)+1;
-    j1 = round(Int,(yi[end]+dyi-Y0)/ry)+1;
-    j=j0:j1;
-
-    b = zeros(length(i),length(j));
-
-    if isglobal
-        i2 = mod.(i .- 1,size(nc["bat"] ,1)) .+ 1;
-        jumps = [0; findall(abs.(i2[2:end]-i2[1:end-1]) .> 1); length(i2)];
-
-        for l=1:length(jumps)-1
-            b[(jumps[l]+1):jumps[l+1],:] = nc["bat"].var[i2[jumps[l]+1]:i2[jumps[l+1]],j];
+        if (maximum(yi) > maximum(y)) || (minimum(yi) < minimum(y))
+            error("data sets '$bath_name' covers only latitude range $(extrema(y)), while latitude range $(extrema(yi)) is requested")
         end
-    else
-        i = maximum([minimum(i) 1]):minimum([maximum(i) length(x)]);
-        j = maximum([minimum(j) 1]):minimum([maximum(j) length(y)]);
-        b[:,:] = nc["bat"].var[i,j];
+        rx = x[2]-x[1];
+        ry = y[2]-y[1];
+        X0 = x[1];
+        Y0 = y[1];
+
+        redx = dxi/rx;
+        redy = dyi/ry;
+
+        i0 = round(Int,(xi[1]-dxi-X0)/rx)+1;
+        i1 = round(Int,(xi[end]+dxi-X0)/rx)+1;
+        i=i0:i1;
+
+        j0 = max(round(Int,(yi[1]-dyi-Y0)/ry)+1,1);
+        j1 = min(round(Int,(yi[end]+dyi-Y0)/ry)+1,length(y));
+        j=j0:j1;
+        b = zeros(length(i),length(j));
+
+        if isglobal
+            i2 = mod.(i .- 1,size(nc["bat"] ,1)) .+ 1;
+            jumps = [0; findall(abs.(i2[2:end]-i2[1:end-1]) .> 1); length(i2)];
+
+            for l=1:length(jumps)-1
+                b[(jumps[l]+1):jumps[l+1],:] = nc["bat"].var[i2[jumps[l]+1]:i2[jumps[l+1]],j];
+            end
+        else
+            i = maximum([minimum(i) 1]):minimum([maximum(i) length(x)]);
+            j = maximum([minimum(j) 1]):minimum([maximum(j) length(y)]);
+            b[:,:] = nc["bat"].var[i,j];
+        end
+
+        bx = X0 .+ rx*(i .- 1)
+        by = Y0 .+ ry*(j .- 1)
+
+        return bx,by,-b
     end
-
-    bx = X0 .+ rx*(i .- 1)
-    by = Y0 .+ ry*(j .- 1)
-
-    return bx,by,-b
 end
 
 """
