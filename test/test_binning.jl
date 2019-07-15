f0afece64307fa85ca6bcb44e84c9fa6b7334a96
Alexander Barth <barth.alexander@gmail.com>
2019-07-15 22:08:18 +0200

    disable test on julia 0.6

--- test/test_binning.jl
+++ test/test_binning.jl
@@ -1,16 +1,15 @@
 if VERSION >= v"0.7"
     using Test
-else
-    using Base.Test
+
+    @test DIVAnd.unstagger([1,2,3]) == [ 0.5,  1.5,  2.5,  3.5]
+
+
+    @test DIVAnd.unstagger(1:3) == 0.5:1.0:3.5
+
+
+    x = 1:0.1:5
+
+    @test DIVAnd.findin(x,2.1234) == 12
+    @test DIVAnd.findin(collect(x),2.1234) == 12
+
 end
-
-@test DIVAnd.unstagger([1,2,3]) == [ 0.5,  1.5,  2.5,  3.5]
-
-
-@test DIVAnd.unstagger(1:3) == 0.5:1.0:3.5
-
-
-x = 1:0.1:5
-
-@test DIVAnd.findin(x,2.1234) == 12
-@test DIVAnd.findin(collect(x),2.1234) == 12
