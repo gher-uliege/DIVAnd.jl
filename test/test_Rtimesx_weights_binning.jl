a67498de15d7354032d176e936a1b9df3a7de46a
Alexander Barth <barth.alexander@gmail.com>
2019-11-08 21:43:25 +0100

    add test_Rtimesx_weights_binning.jl

---
+++ test/test_Rtimesx_weights_binning.jl
@@ -0,0 +1,55 @@
+import DIVAnd
+using Test
+using DelimitedFiles
+using DataStructures
+using Missings
+using NCDatasets
+
+#
+varname = "Salinity"
+filename = "WOD-Salinity.nc"
+
+
+bathname = joinpath(
+    dirname(@__FILE__),
+    "..",
+    "..",
+    "DIVAnd-example-data",
+    "Global",
+    "Bathymetry",
+    "gebco_30sec_16.nc",
+)
+bathisglobal = true
+
+obsname = joinpath(
+    dirname(@__FILE__),
+    "..",
+    "..",
+    "DIVAnd-example-data",
+    "Provencal",
+    "WOD-Salinity.nc",
+)
+
+cdilist = joinpath(dirname(@__FILE__), "..", "data", "CDI-list-export.csv")
+
+
+if !isfile(bathname)
+    @info("download bathymetry $bathname")
+    bathname = download("https://dox.ulg.ac.be/index.php/s/U0pqyXhcQrXjEUX/download")
+end
+
+
+if !isfile(obsname)
+    @info("download observations $obsname")
+    obsname = download("https://dox.ulg.ac.be/index.php/s/PztJfSEnc8Cr3XN/download")
+end
+
+obsvalue, obslon, obslat, obsdepth, obstime, obsids = DIVAnd.loadobs(
+    Float64,
+    obsname,
+    "Salinity",
+)
+
+
+x = (obslon,obslat)
+
