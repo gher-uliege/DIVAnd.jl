a1e1dff1fffe8a96d997232c24b4deba7bff0b87
Alexander Barth <barth.alexander@gmail.com>
2020-01-31 09:57:41 +0100

    factor-out DIVAnd_adaptedeps2.jl

--- src/DIVAnd_adaptedeps2.jl
+++ src/DIVAnd_adaptedeps2.jl
@@ -18,26 +18,44 @@ estimate of the R matrix. If you cannot use `DIVAndrun` but use `DIVAndgo`, the 
 function DIVAnd_adaptedeps2(s, fi)
 
     residual = DIVAnd_residualobs(s, fi)
-
-    d0d = zero(eltype(fi))     # yo ⋅ yo
-    d0dmd1d = zero(eltype(fi)) # (yo - Hxa) ⋅ yo
     diagR = diag(s.obsconstrain.R)::Vector{Float64}
-    eps2 = zero(eltype(fi))    # mean of diagonal of R
+
+    return DIVAnd_adaptedeps2(s.yo, residual, diagR, s.obsout)
+end
+
+
+"""
+    DIVAnd_adaptedeps2(yo, residual, diagR, obsout)
+
+Using Deroziers adaptive approach provides a multiplicative factor for the current epsilon2 value so that factor*epsilon2 is a better
+estimate of the R matrix.
+
+`yo` the observations (minus the background),
+`residual` the obserations minus the analysis,
+`diagR`, the diagonal of the obs. error covariance matrix and
+`obsout` is true if an observation is out of the grid.
+"""
+function DIVAnd_adaptedeps2(yo, residual, diagR, obsout)
+    d0d = zero(eltype(yo))     # yo ⋅ yo
+    d0dmd1d = zero(eltype(yo)) # (yo - Hxa) ⋅ yo
+    eps2 = zero(eltype(yo))    # mean of diagonal of R
     nrealdata = 0
 
-    for i = 1:length(s.yo)
-        if (!s.obsout[i]) && (!isinf(diagR[i]))
-            d0d += s.yo[i]^2
-            d0dmd1d += s.yo[i] * residual[i]
+    for i = 1:length(yo)
+        if (!obsout[i]) && (!isinf(diagR[i]))
+            d0d += yo[i]^2
+            d0dmd1d += yo[i] * residual[i]
             eps2 += diagR[i]
             nrealdata += 1
         end
     end
+
     eps2 /= nrealdata
 
     ll1 = d0d / (d0dmd1d) - 1
     eps1 = 1 / ll1
     factor = eps1 / eps2
+
     return factor
 end
 
