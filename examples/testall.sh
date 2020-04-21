6029b85f6a9e108f04cd93d8138264ae8ae6f6b3
Alexander Barth <barth.alexander@gmail.com>
2020-04-21 12:19:44 +0200

    fix test script for examples

--- examples/testall.sh
+++ examples/testall.sh
@@ -1,1 +1,6 @@
-for i in *.jl; do sbatch  $(awk  /#SBATCH/ { print } $i) --job-name=${i%.jl} --output=${i%.jl}-%j.log submit_julia_1cpu.sh $i; done
+# test all tests with SLURM starting with DIVAnd
+
+for i in DIVAnd*.jl; do
+    echo submit $i
+    sbatch  $(awk  '/#SBATCH/ { print $2 }'  $i) --job-name=${i%.jl} --output=${i%.jl}-%j.log submit_julia_1cpu.sh $i; 
+done
